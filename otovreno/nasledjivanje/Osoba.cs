886b2df7ed378b2b63d702f4c21ac91d9cbe4de1
Ogyshaa <ognjenradojkovic7@gmail.com>
2024-10-17 23:08:53 +0200

    Add files via upload

---
+++ otovreno/nasledjivanje/Osoba.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nasledjivanje312
+{
+    class Osoba
+    {
+        protected string ime;
+        protected int godina;
+        public Osoba(string ime, int god)
+        {
+            this.ime = ime;
+            godina = god;
+        }
+        public virtual void PredstaviSe()
+        {
+            Console.WriteLine(ime + " " + godina);
+        }
+    }
+    class Ucenik : Osoba
+    {
+        private int razred;
+        private double prosek;
+        public Ucenik(string ime, int god, 
+            int razred, double prosek) : base(ime, god)
+        {
+            this.razred = razred;
+            this.prosek = prosek;
+        }
+        public override void PredstaviSe()
+        {
+            Console.WriteLine(ime + " " + godina + " " + razred +" "+ prosek);
+        }
+    }
+    
+
+    class Nastavnik : Osoba
+    {
+        private double neto;
+        public Nastavnik(string ime, int godina, double plata):base(ime, godina)
+        {
+            neto = plata;
+        }
+        public override void PredstaviSe()
+        {
+            Console.WriteLine(ime + " " + godina +" "+ neto);
+        }
+
+    }
+
+
+}
---
+++ otovreno/nasledjivanje/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nasledjivanje312
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba o1 = new Osoba("Marko Markovic", 2008);
+            Ucenik u1 = new Ucenik("Janko Jankvoic", 2007,3,4.5)  ;
+            Nastavnik n1 = new Nastavnik("Petar Petrovic",1980,90000);
+            o1.PredstaviSe();
+            u1.PredstaviSe();
+            n1.PredstaviSe();
+            Console.ReadLine();
+
+            Osoba[] osobe = { o1, n1, n1, u1, o1, u1 };
+            for (int i = 0; i < osobe.Length;i++)
+            {
+                osobe[i].PredstaviSe();
+            }
+            Console.ReadLine();
+        }
+    }
+}
