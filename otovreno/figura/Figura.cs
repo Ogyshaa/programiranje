27a18506fdd9d7b268a46fb24b0c4b5303dd6853
Ogyshaa <ognjenradojkovic7@gmail.com>
2024-10-17 22:58:46 +0200

    Add files via upload

---
+++ otovreno/figura/Figura.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace figure312
+{
+    class Figura
+    {
+        protected double dimenzaija;
+        protected double obim;
+        protected double povrsina;
+        public Figura(double d)
+        {
+            dimenzaija = d;
+        }
+        public virtual void Racunaj()
+        {
+            povrsina = 0;
+            obim = 0;
+        }
+        public string Ispis()
+        {
+            return "P= " + povrsina.ToString() + "O= " + obim.ToString();
+        }
+    }
+    class Krug : Figura
+    {
+        public Krug(double dim) : base(dim)
+        {
+           
+        }
+        public override void Racunaj()
+        {
+            povrsina = Math.Round(Math.Pow(dimenzaija, 2) * Math.PI,2);
+            obim = Math.Round(2 * dimenzaija * Math.PI,2);
+        }
+
+    }
+    class Kvadrat : Figura
+    {
+        public Kvadrat(double dim) : base(dim)
+        { }
+        public override void Racunaj()
+        {
+            povrsina = Math.Pow(dimenzaija, 2);
+            obim = 4 * dimenzaija;
+        }
+    }
+
+}
---
+++ otovreno/figura/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace figure312
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblraz_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnrac_Click(object sender, EventArgs e)
+        {
+            double a;
+            Figura f;
+            Krug kr;
+            Kvadrat kv;
+            if (!double.TryParse(txtb.Text, out a))
+            {
+                MessageBox.Show("Pogrsan podatak ):");
+                txtb.Text = "";
+                txtb.Focus();
+            }
+            if (Krug.Checked)
+            {
+                kr = new Krug(a);
+                f = kr;
+            }
+            else
+            {
+                kv = new Kvadrat(a);
+                f = kv;
+            }
+            f.Racunaj();
+            lblraz.Text = f.Ispis();
+        }
+    }
+}
