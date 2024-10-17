84095bd1fb5a923e47ab09d027f52cec3282dd06
Ogyshaa <ognjenradojkovic7@gmail.com>
2024-10-17 23:10:15 +0200

    Add files via upload

---
+++ otovreno/WinTrougao/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinTrougao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        Trougao t;
+        double a, b, c;
+
+        private void txtb_Leave(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txtb.Text, out b))
+            {
+                txtb.Text = "";
+                txtb.Focus();
+            }
+        }
+
+        private void txtc_Leave(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txtc.Text, out c))
+            {
+                txtc.Text = "";
+                txtc.Focus();
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!double.TryParse(textBox1.Text, out a))
+            {
+                textBox1.Text = "";
+                textBox1.Focus();
+            }
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (!double.TryParse(textBox1.Text, out a))
+            {
+                textBox1.Text = "";
+                textBox1.Focus();
+            }
+        }
+
+        private void btnRcunaljka_Click(object sender, EventArgs e)
+        {
+            if (a + b > c && b + c > a && c + a > b)
+            {
+                t = new Trougao(a, b, c);
+                double o, p;
+                t.ObimP(out o, out p);
+                lblO.Text = o.ToString();
+                lblP.Text = p.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Stranice ne čine trougao )); !!!");
+                textBox1.Focus();
+            }
+        }
+    }
+}
---
+++ otovreno/WinTrougao/Trougao.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinTrougao
+{
+    class Trougao
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public double A
+        {
+            get { return a; }
+            set{a= value;}
+        }
+        public double B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+        public double C
+        {
+            get { return c; }
+            set { c = value; }
+        }
+        public Trougao()
+        {
+            a = 0; b = 0; c = 0;
+        }
+        public Trougao(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public void ObimP(out double O, out double P)
+        {
+            double s;
+            s = (a + b + c) / 2;
+            P = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            O = a + b + c;
+        }
+    }
+}
