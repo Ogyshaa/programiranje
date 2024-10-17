426d6b3c46581c664a0f56f90295ed77fa5869cf
Ogyshaa <ognjenradojkovic7@gmail.com>
2024-10-17 23:03:22 +0200

    Add files via upload

---
+++ otovreno/kompleksni brojevi/Kompleksni.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kompleksni_br
+{
+    class Kompleksni
+    {
+        private double x;
+        private double y;
+    
+        public double X
+            {
+                get{return x;}
+                set{x=value;}
+            }
+               public double Y
+            {
+                get{return y;}
+                set{y=value;}
+            }
+        public Kompleksni()
+{
+    x = 0;
+    y = 0;
+
+}
+public Kompleksni(double x, double y)
+{
+    this.x = x;
+    this.y = y;
+}
+public Kompleksni Zbir(Kompleksni z1)
+{
+    Kompleksni z = new Kompleksni();
+    z.x = this.x + z1.x;
+    z.y = this.y + z1.y;
+    return z;
+}
+public void Ispisati()
+{
+    Console.WriteLine("z=" + x + "i" + y);
+}
+    }
+}
---
+++ otovreno/kompleksni brojevi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kompleksni_br
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kompleksni a, b, c;
+            a = new Kompleksni(5, 10);
+            b = new Kompleksni(2, 3);
+            c = new Kompleksni();
+            c = a.Zbir(b);//b.Zbir(a);
+            a.Ispisati();
+            b.Ispisati();
+            c.Ispisati();
+            Console.ReadLine();
+        }
+    }
+}
