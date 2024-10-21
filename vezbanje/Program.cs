4b3fdd8a13d720e2f14b78bb41042d9363ba3ac5
Ogyshaa <ognjenradojkovic7@gmail.com>
2024-10-21 18:48:07 +0200

    Add files via upload

---
+++ vezbanje/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Racun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Gorivo gorivo1 = new Gorivo(1, 150.0, "Benzin", "l", 50.0, 20.0);
+            Gorivo gorivo2 = new Gorivo(2, 140.0, "Dizel", "l", 60.0, 15.0);
+            Gorivo gorivo3 = new Gorivo(3, 130.0, "Plin", "l", 40.0, 10.0);
+            
+
+            // Prikazujemo informacije o svakom gorivu pre promene cene
+            Console.WriteLine("Informacije o gorivu pre promene cene:");
+            gorivo1.Prikazi();
+            gorivo2.Prikazi();
+            gorivo3.Prikazi();
+            
+            // Menjamo cenu za 2% za svako gorivo
+            gorivo1.PromeniCenu(2);
+            gorivo2.PromeniCenu(2);
+            gorivo3.PromeniCenu(2);
+            
+            // Prikazujemo informacije o svakom gorivu nakon promene cene
+            Console.WriteLine("\nInformacije o gorivu posle promene cene za 2%:");
+            gorivo1.Prikazi();
+            gorivo2.Prikazi();
+            gorivo3.Prikazi();
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ vezbanje/Roba.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Racun
+{
+    internal class Roba
+    {
+        private int sifra;
+        private double cena;
+        private string naziv;
+        private string jedMere;
+        private double kolicina;
+
+        // Podrazumevani konstruktor
+        public Roba()
+        {
+            sifra = 0;
+            cena = 0.0;
+            naziv = "Nepoznato";
+            jedMere = "kg";
+            kolicina = 0.0;
+        }
+
+        // Sopstveni konstruktor
+        public Roba(int sifra, double cena, string naziv, string jedMere, double kolicina)
+        {
+            this.sifra = sifra;
+            this.cena = cena;
+            this.naziv = naziv;
+            this.jedMere = jedMere;
+            this.kolicina = kolicina;
+        }
+
+        // Getter i Setter za Sifra
+        public int Sifra
+        {
+            get{ return sifra; }
+            set{ sifra = value;  }
+        }
+
+        // Getter i Setter za Cena
+        public double Cena
+        {
+            get { return cena; } set { cena = value; }
+        }
+
+        // Getter i Setter za Naziv
+        public string Naziv
+        {
+            get { return naziv; }
+            set { naziv = value; }
+        }
+
+        // Getter i Setter za JedMere
+        public string JedMere
+        {
+            get { return jedMere; }
+            set { jedMere = value; }
+        }
+
+        // Getter i Setter za Kolicina
+        public double Kolicina
+        {
+            get { return kolicina; }
+            set { kolicina = value; }
+        }
+
+        // Metoda za promenu cene
+        public virtual void PromeniCenu(double procenatPromene)
+        {
+            cena += cena * procenatPromene / 100;
+        }
+
+        // Metoda za prikazivanje informacija o robi
+        public virtual void Prikazi()
+        {
+            Console.WriteLine($"Sifra: {sifra}, Naziv: {naziv}, Cena: {cena} RSD, Količina: {kolicina} {jedMere}");
+        }
+    }
+
+    // Izvedena klasa Gorivo
+    class Gorivo : Roba
+    {
+        private double akciza;
+
+        // Konstruktor
+        public Gorivo(int sifra, double cena, string naziv, string jedMere, double kolicina, double akciza) : base(sifra, cena, naziv, jedMere, kolicina)
+        {
+            this.akciza = akciza;
+        }
+
+        // Getter i Setter za Akciza
+        public double Akciza
+        {
+            get { return akciza; }
+            set { akciza = value; }
+        }
+        // Pregazena metoda za promenu cene
+        public override void PromeniCenu(double procenatPromene)
+        {
+            base.PromeniCenu(procenatPromene); // Promena po procentu
+            Cena+=akciza; // Dodavanje akcize na cenu
+        }
+
+        // Pregazena metoda za prikazivanje informacija o gorivu
+        public override void Prikazi()
+        {
+            Console.WriteLine($"Sifra: {Sifra}, Naziv: {Naziv}, Cena sa akcizom: {Cena} RSD, Akciza: {akciza} RSD, Količina: {Kolicina} {JedMere}");
+        }
+    }
+}
