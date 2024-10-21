2e5e94814f7e78c7d0df09ee618455c1e203451e
NemanjaKing <anjamen213@gmail.com>
2024-10-21 20:13:21 +0200

    Add files via upload

---
+++ NemanjaZbaVež/NemanjaZbaVež/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NemanjaZbaVež
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Gorivo[] gorivos = new Gorivo[5];
+            gorivos[0] = new Gorivo(505,  "dijabola", "m", 16, 100,20);
+            gorivos[1] = new Gorivo(545, "dijabla", "m", 56, 100, 24);
+            gorivos[2] = new Gorivo(535, "dibola", "m", 46, 100, 23);
+            gorivos[3] = new Gorivo(515, "ola", "m", 36,100, 22);
+            gorivos[4] = new Gorivo(525, "jabola", "m", 26,100, 21);
+
+            for (int i = 0; i < gorivos.Length; i++)
+            {
+                gorivos[i].Prikazi();
+                gorivos[i].Promena(2);
+            }
+            for(int i = 0; i < gorivos.Length; i++)
+            {
+                gorivos[i].Prikazi();
+            }
+            Console.ReadLine();
+        }      
+    }
+}
---
+++ NemanjaZbaVež/NemanjaZbaVež/Roba.cs
@@ -0,0 +1,66 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NemanjaZbaVež
+{
+    internal class Roba
+    {
+        protected int sifra;
+        protected string naziv, jed_mere;
+        protected double kolicina, cena;
+
+        public Roba()
+        {
+            sifra = 0;
+            naziv= jed_mere = "";
+            kolicina = cena = 0;
+        }
+        public Roba (int sifra, string naziv, string jed_mere, double kolicina, double cena)
+        {
+            this.sifra = sifra; 
+            this.naziv = naziv; 
+            this.jed_mere= jed_mere;    
+            this.kolicina= kolicina;   
+            this.cena= cena;
+        }
+
+        public virtual void Promena(double procenat)
+        {
+            cena += cena * procenat / 100;
+        }
+        public virtual void Prikazi()
+        {
+            Console.WriteLine("Sifra:" + sifra + "naziv:" + naziv + "jed_mere" + jed_mere + "kolicina" + kolicina + "cena" + cena);
+        }
+    }
+    class Gorivo : Roba
+    {
+        private double proc;
+
+        public double Proc
+        {
+            get { return proc; }
+            set { proc = value; }
+        }
+
+        public Gorivo(int sifra, string naziv, string jed_mere, double kolicina,double cena,double proc):base(sifra, naziv, jed_mere,kolicina,cena)
+        {
+            this.proc= proc;  
+        }
+        public override void Promena(double procenat)
+        {
+            cena += cena * procenat / 100;
+            cena += cena * proc / 100;
+        }
+        public override void Prikazi()
+        {
+            Console.WriteLine("Sifra:   " + sifra + "   naziv: " + naziv + "    jed_mere:   " + jed_mere + "    kolicina   " + kolicina + " cena    " + cena+"  proc:     "+proc);
+        }
+    }
+}
