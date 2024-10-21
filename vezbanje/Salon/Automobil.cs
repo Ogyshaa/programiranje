46de62e3ad484da3487a04a7048e708effdb2a16
Ogyshaa <ognjenradojkovic7@gmail.com>
2024-10-21 21:46:28 +0200

    Add files via upload

---
+++ vezbanje/Salon/Automobil.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salon
+{
+    internal class Automobil
+    {
+        private int id;
+        private string marka;
+        private string model;
+        private double cena;
+        private double km;
+
+        public Automobil()
+        {
+            id = 0;
+            marka = "lel";
+            model = "lel";
+            cena = 0;
+            km = 0;
+        }
+
+        public Automobil(int id, string marka, string model, double cena, double km)
+        {
+            this.id = id;
+            this.marka = marka;
+            this.model = model;
+            this.cena = cena;
+            this.km = km;
+        }
+
+        public int Id
+        { 
+            get { return id; }  
+            set { id = value; }
+        }
+        public string Marka
+            { get { return marka; } set { marka = value; } }
+        public string Model 
+            { get { return model; } set { model = value; } }
+        public double Cena
+            { get { return cena; } set { cena = value; } }
+        public double Km
+            { get { return km; } set { km = value; } }
+
+        public virtual void PromCena(double promena)
+        {
+            cena += cena * promena / 100;
+        }
+
+        public virtual void Ispis()
+        {
+            Console.WriteLine($"ID: {id}, marka: {marka}, model: {model}, cena: {cena} EURO, kilometraza {km } km");
+        }
+    }
+    class ElektricniAuto : Automobil
+    {
+        private double kapacitetbat;
+
+        public double KapacitetBat
+        {
+            get { return kapacitetbat; } set { kapacitetbat = value; }
+        }
+        public ElektricniAuto(int id, string marka, string model, double cena, double km, double kapacitetbat) : base(id, marka, model, cena, km)
+        {
+            this.kapacitetbat = kapacitetbat;
+        }
+
+        public override void PromCena(double promena)
+        {
+            base.PromCena(promena);
+            Cena += Cena * 0.10;
+        }
+
+        public override void Ispis()
+        {
+            base.Ispis();
+            Console.WriteLine($"kapacitet baterije {kapacitetbat} kWh");
+        }
+    }
+}
---
+++ vezbanje/Salon/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Automobil lol1= new Automobil(1, "Audi", "S3", 4500, 200000);
+            Automobil lol2 = new Automobil();
+            ElektricniAuto lel1 = new ElektricniAuto(2, "Audi", "Etron", 20000, 125000, 95);
+
+            lol1.Ispis();
+            lol2.Ispis();
+            lel1.Ispis();
+
+            lol1.PromCena(10);
+            lol2.PromCena(10);
+            lel1.PromCena(20);
+
+            Console.WriteLine("\nPosle Promen");
+            lol1.Ispis();
+            lol2.Ispis();
+            lel1.Ispis();
+            
+            Console.ReadLine();
+
+        }
+    }
+}
